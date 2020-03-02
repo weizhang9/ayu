a3cc33007d6d0942ceb7b81002bc8e79dab1313f
weizhang9 <wei.zhang@sky.uk>
2020-03-02 12:36:06 +0000

    made -L optional and added support for all-caps and non-caps flags

--- readme.md
+++ readme.md
@@ -1,23 +1,50 @@
 This is a command line tool to generate random password.
 
 ## How to install
-### *via [crates.io](https://crates.io/crates/ayu)*
+### *via [crates.io](https://crates.io/crates/ayu)* (supports 32-bit & 64-bit Windows, Linux and MacOS)
 1. [install Cargo](https://doc.rust-lang.org/cargo/getting-started/installation.html) if you haven't already done so.
 > `brew install rust` on MacOS will get you rustup and cargo
 2. run `cargo install ayu`
 
-### *via downloading binary*
+### *via downloading binary* (supports 64-bit Linux and MacOS and 32-bit & 64-bit Windows)
 1. go to [releases](https://github.com/weizhang9/ayu/releases) section of the repo and download the desired binary
-5. put the binary at your desired location and add the `path/to/ayu` to your $PATH in .bashrc
+5. put the binary at your desired location and add the `path/to/ayu` to your `$PATH` variable in your .bashrc file
 
 ## Basic usage
-- Run `ayu --help` to see all the information.
+- Run `ayu -h` to see all the information.
+```
+USAGE:
+    ayu [FLAGS] [OPTIONS]
+
+FLAGS:
+    -N, --lowercase    Return password in LOWERCASE, invalid if used with UPPERCASE flag -U
+    -U, --uppercase    Return password in UPPERCASE, invalid if used with LOWERCASE flag -N
+    -h, --help         Prints help information
+    -V, --version      Prints version information
+
+OPTIONS:
+    -C, --char <CHARSET>    [OPTIONAL] desired password CHARSET
+                            Options include:
+                            1) Alphanumeric + Special characters (default)
+                            2) Alphanumeric
+                            3) Alphabetic
+                            4) Numberic
+
+    -L, --len <LENGTH>      [OPTIONAL] desired password LENGTH (default: 18)
+```
 
 - Example: 
 ```
-ayu -L=20 -C=1
+ayu
 ```
 
-> This command generates you a random password of a length of 20 and it contains alphameric + special charset, which is the default charset. 
+> This command generates a random password of a length of 18 and it contains alphameric + special charset, which is the default charset. 
 
-> `-L` flag is required, `-C` is optional. There are 4 options to choose from. 
+```
+ayu -L=20 -C=2 -U
+```
+
+> This command generates a random password of a length of 20 and it contains uppercase alphameric.
+
+## Maintenance and Contribution
+This tool is passively maintained. If you encountered an issue or have any suggestions, please submit [here](https://github.com/weizhang9/ayu/issues). Contributions are always welcome. :)
--- src/main.rs
+++ src/main.rs
@@ -11,43 +11,76 @@ fn run() -> String {
     (version: crate_version!())
     (author: "Wei Zhang <vivian_17@msn.cn>")
     (about: "Random password generator")
-    (@arg LENGTH: -L --len  +required +takes_value "(REQUIRED) desired password LENGTH")
-    (@arg CHARSET: -C --char +takes_value "(OPTIONAL) desired password CHARSET\n\
+    (@arg LENGTH: -L --len  +takes_value "[OPTIONAL] desired password LENGTH (default 18 characters)")
+    (@arg CHARSET: -C --char +takes_value "[OPTIONAL] desired password CHARSET\n\
     Options include:\n\
     1) Alphanumeric + Special characters (default)\n\
     2) Alphanumeric\n\
     3) Alphabetic\n\
     4) Numberic")
+    (@arg UPPERCASE: -U --uppercase "Return password in UPPERCASE, invalid if used with LOWERCASE flag -N")
+    (@arg LOWERCASE: -N --lowercase "Return password in LOWERCASE, invalid if used with UPPERCASE flag -U")
     )
     .get_matches_safe().unwrap_or_else(|e| e.exit());
 
-    let password_len = args.value_of("LENGTH").unwrap();
-    if args.is_present("CHARSET") {
+    let allcaps = args.is_present("UPPERCASE");
+    let noncaps = args.is_present("LOWERCASE");
+    if args.is_present("CHARSET") && args.is_present("LENGTH") {
         let option = args.value_of("CHARSET").unwrap();
-        generate_with_option(password_len, option.parse().unwrap())
+        let password_len = args.value_of("LENGTH").unwrap();
+        generate_with_option(password_len, option.parse().unwrap(), allcaps, noncaps)
+    } else if args.is_present("LENGTH") {
+        let password_len = args.value_of("LENGTH").unwrap();
+        generate_with_option(password_len, 1, allcaps, noncaps)
+    } else if args.is_present("CHARSET") {
+        let option = args.value_of("CHARSET").unwrap();
+        generate_with_option("18", option.parse().unwrap(), allcaps, noncaps)
     } else {
-        generate(password_len)
+        generate_with_option("18", 1, allcaps, noncaps)
     }
 }
 
-fn generate_with_option(len: &str, option: u8) -> String {
+fn generate_with_option(len: &str, option: u8, allcaps: bool, noncaps: bool) -> String {
     use rand::Rng;
-    const ALPHANUMSPEC: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                            abcdefghijklmnopqrstuvwxyz\
-                            0123456789!()-.?[]_`~;:!@#$%^&*+=";
-    const ALPHANUM: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                        abcdefghijklmnopqrstuvwxyz\
-                        0123456789";
-    const ALPHA: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                        abcdefghijklmnopqrstuvwxyz";
-    const NUM: &[u8] = b"0123456789"; 
+    const UPPERCASE: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const LOWERCASE: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+    const SPECIAL: &[u8] = b"()-.?[]_`~;:!@#$%^&*+=";
+    const NUM: &[u8] = b"0123456789";
+    // const ALPHANUMSPEC: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    //                         abcdefghijklmnopqrstuvwxyz\
+    //                         0123456789!()-.?[]_`~;:!@#$%^&*+=";
+    // const ALPHANUM: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    //                     abcdefghijklmnopqrstuvwxyz\
+    //                     0123456789";
+    // const ALPHA: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+    //                     abcdefghijklmnopqrstuvwxyz";
+    // const NUM: &[u8] = b"0123456789"; 
 
-    let charset: &[u8] = match option {
-        1 => ALPHANUMSPEC,
-        2 => ALPHANUM,
-        3 => ALPHA,
-        4 => NUM,
-        _ => ALPHANUMSPEC,
+    let allchars: &[u8] = &[UPPERCASE, LOWERCASE, SPECIAL, NUM].concat();
+    let allchars_caps: &[u8] = &[UPPERCASE, SPECIAL, NUM].concat();
+    let allchars_noncaps: &[u8] = &[LOWERCASE, SPECIAL, NUM].concat();
+    let alphameric: &[u8] = &[UPPERCASE, LOWERCASE, NUM].concat();
+    let alphameric_caps: &[u8] = &[UPPERCASE, NUM].concat();
+    let alphameric_noncaps: &[u8] = &[LOWERCASE, NUM].concat();
+    let alphabets: &[u8] = &[UPPERCASE, LOWERCASE].concat();
+
+    let charset: &[u8] = match (option, allcaps, noncaps) {
+        (1, true, true) => allchars,
+        (1, true, false) => allchars_caps,
+        (1, false, true) => allchars_noncaps,
+        (1, false, false) => allchars,
+        (2, true, true) => alphameric,
+        (2, true, false) => alphameric_caps,
+        (2, false, true) => alphameric_noncaps,
+        (2, false, false) => alphameric,
+        (3, true, true) => alphabets,
+        (3, true, false) => UPPERCASE,
+        (3, false, true) => LOWERCASE,
+        (3, false, false) => alphabets,
+        (4, _, _) => NUM,
+        (_, true, false) => allchars_caps,
+        (_, false, true) => allchars_noncaps,
+        (_, _, _) => allchars,
     };
     let mut rng = rand::thread_rng();
 
@@ -55,23 +88,6 @@ fn generate_with_option(len: &str, option: u8) -> String {
         .map(|_| {
             let idx = rng.gen_range(0, charset.len());
             charset[idx] as char
-        })
-        .collect();
-    password
-}
-
-fn generate(len: &str) -> String {
-    use rand::Rng;
-    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
-                            abcdefghijklmnopqrstuvwxyz\
-                            0123456789!()-.?[]_`~;:!@#$%^&*+=";
-
-    let mut rng = rand::thread_rng();
-
-    let password: String = (0..len.parse().unwrap())
-        .map(|_| {
-            let idx = rng.gen_range(0, CHARSET.len());
-            CHARSET[idx] as char
         })
         .collect();
     password
